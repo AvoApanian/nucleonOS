0cae4ef5147f3d8ec8cf2201ed2b28d91efff5da
AvoApanian <apanianavo3@gmail.com>
2026-07-16 17:37:35 +0400

    boatloader complete No Os

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,123 +1,75 @@
 org 0x7C00
 bits 16
-
-
 start:
     cli
-
     xor ax,ax
     mov ds,ax
-
     mov es,ax
     mov [bootDrive],dl
-
-   call loadStage2
-
+    call loadStage2
     lgdt [gdtDescriptor]
-
     mov eax,cr0
     or eax,1
     mov cr0,eax
-
     o32 jmp 0x08:protected_mode
-
 bits 32
-
 protected_mode:
     mov ax,0x10
-
     mov ds,ax
     mov es,ax
     mov ss,ax
-
     mov edi,0xB8000
     mov ecx,2000
-
-
 clearScreen:
-
     mov word [edi],0x0020
     add edi,2
-
     loop clearScreen
-
     mov esi,message
     mov edi,0xB8000 + ((12*80+30)*2)
-
 print:
     mov al,[esi]
-
     cmp al,0
     je stage2
-
     mov [edi],al
     mov byte [edi+1],0x0F
-
     add esi,1
     add edi,2
-
     jmp print
-
 stage2:
-	jmp 0x8000
-
+    jmp 0x8000
 message:
     db "NucleonOS Welcome :)",0
-
-
 bits 16
-
-
 gdStart:
     dq 0
-
-    ; Kernel Code Ring 0
-    ; Base 0
-    ; Limit 4GB
-    ; Execute/Read
     dq 0x00CF9A000000FFFF
-
-
-
-    ; Kernel Data Ring 0
-    ; Base 0
-    ; Limit 4GB
-    ; Read/Write
     dq 0x00CF92000000FFFF
-
 gdt_end:
-
 gdtDescriptor:
-
     dw gdt_end - gdStart - 1
     dd gdStart
 
 loadStage2:
-
-    mov ah,0x02
-    mov al,1
-    mov ch,0
-    mov cl,2
-    mov dh,0
+    mov si,dap
+    mov ah,0x42
     mov dl,[bootDrive]
-
-    mov bx,0x8000
-
     int 0x13
-
     jc diskError
-
     ret
-
-
 diskError:
     cli
     hlt
     jmp diskError
 
+dap:
+    db 0x10
+    db 0
+    dw 40
+    dw 0x8000
+    dw 0x0000
+    dq 1
 
 bootDrive:
     db 0
-
-times 510-($-$$) db 0 
+times 510-($-$$) db 0
 dw 0xAA55
--- bootloader/stage2.asm
+++ bootloader/stage2.asm
@@ -1,17 +1,118 @@
 org 0x8000
-
 bits 32
-
 protected_mode:
+    mov esi,msgProtected
+    mov edi,0xB8000          
+printProtected:
+    lodsb
+    test al,al
+    jz protectedDone
+    mov [edi],al
+    inc edi
+    mov byte [edi],0x07
+    inc edi
+    jmp printProtected
+protectedDone:
+    mov eax,cr4
+    or eax,1<<5
+    mov cr4,eax
 
-    mov edi,0xB8000
+    ; PML4[0] -> pdpt
+    mov eax,pdpt
+    or eax,0b11
+    mov dword [pml4],eax
+    mov dword [pml4+4],0
 
-    ; caractère A
-    mov byte [edi], 'A'
+    ; PDPT[0] -> pd
+    mov eax,pd
+    or eax,0b11
+    mov dword [pdpt],eax
+    mov dword [pdpt+4],0
 
-    ; couleur cyan
-    mov byte [edi+1], 0x0B
+    ; PD[0] -> pt
+    mov eax,pt
+    or eax,0b11
+    mov dword [pd],eax
+    mov dword [pd+4],0
 
+    xor ecx,ecx
+fillPt:
+    mov eax,ecx
+    shl eax,12
+    or eax,0b11
+    mov dword [pt + ecx*8], eax
+    mov dword [pt + ecx*8 + 4], 0
+    inc ecx
+    cmp ecx,512
+    jne fillPt
 
+    mov eax,pml4
+    mov cr3,eax
+
+    mov ecx,0xC0000080
+    rdmsr
+    or eax,1<<8
+    wrmsr
+
+    lgdt [gdt64.Descriptor]
+
+    mov eax,cr0
+    or eax,1<<31
+    mov cr0,eax
+
+    jmp dword 0x08:longMode
+
+bits 64
+longMode:
+    mov ax,0x10
+    mov ds,ax
+    mov es,ax
+    mov ss,ax
+    xor eax,eax
+    mov fs,ax
+    mov gs,ax
+
+    mov rsp,0x90000
+
+    mov rsi,msgLong
+    mov rdi,0xB8000 + 160    
+printLong:
+    lodsb
+    test al,al
+    jz halt
+    mov [rdi],al
+    inc rdi
+    mov byte [rdi],0x07
+    inc rdi
+    jmp printLong
 halt:
     jmp halt
+
+msgProtected db "Protected Mode OK",0
+msgLong db "Long Mode Activated",0
+
+align 16
+gdt64:
+    dq 0
+.code: equ $ - gdt64
+    dq 0x00209A0000000000
+.data: equ $ - gdt64
+    dq 0x0000920000000000
+.Descriptor:
+    dw $ - gdt64 - 1
+    dd gdt64
+
+align 4096
+pml4:
+    times 512 dq 0
+align 4096
+pdpt:
+    times 512 dq 0
+align 4096
+pd:
+    times 512 dq 0
+align 4096
+pt:
+    times 512 dq 0
+
+times 40*512 - ($ - $$) db 0
